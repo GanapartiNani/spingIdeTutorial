32a9600034ff4f1e89d34a5b1f853c0c36293d74
tatabbai <ganaparthinani@gmail.com>
2024-12-30 16:04:01 +0000

    democode

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloWorldController{
+
+@GetMapping("/")
+public String getWelcome(){
+    return "Hello World";
+}
+
+@GetMapping("/name")
+public String getName(){
+    return "Nani";
+}
+}
